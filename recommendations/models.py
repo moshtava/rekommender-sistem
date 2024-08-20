a1bce353bdda814e0958b094c5f96575d2507bb6
Mojtaba Negahdari <contact.mojtaba.negahdari@gmail.com>
2024-08-21 01:43:43 +0330

    added model for cryptocurrency

--- recommendations/models.py
+++ recommendations/models.py
@@ -4,3 +4,8 @@ class User(models.Model):
     username = models.CharField(max_length=100)
     mobile_number = models.CharField(max_length=15)
     email = models.EmailField()
+    
+class Cryptocurrency(models.Model):
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=10)
+    
