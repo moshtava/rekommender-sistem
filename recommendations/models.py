8f1760267936de35cc0364ad14f1cd30bec679e3
Mojtaba Negahdari <contact.mojtaba.negahdari@gmail.com>
2024-08-21 04:29:29 +0330

    deleted User, Cryptocurrency, and Transaction model classes.

--- recommendations/models.py
+++ recommendations/models.py
@@ -1,18 +1,1 @@
 from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    mobile_number = models.CharField(max_length=15)
-    email = models.EmailField()
-    
-class Cryptocurrency(models.Model):
-    name = models.CharField(max_length=100)
-    symbol = models.CharField(max_length=10)
-
-class Transaction(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    crypto = models.ForeignKey(Cryptocurrency, on_delete=models.CASCADE)
-    transaction_type = models.CharField(max_length=10)
-    amount = models.FloatField()
-    date = models.DateTimeField()    
-    
