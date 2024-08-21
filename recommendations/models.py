85251cc1ba530d02b605c2f3b92833f174fa248d
Mojtaba Negahdari <contact.mojtaba.negahdari@gmail.com>
2024-08-21 04:30:30 +0330

    added a model class named Transaction

--- recommendations/models.py
+++ recommendations/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+class Transaction(models.Model):
+   user_id = models.IntegerField()
+   crypto_id = models.IntegerField()
+   transaction_type = models.CharField(max_length=4)
+   transaction_amount = models.FloatField()
+   transaction_date = models.DateTimeField()
+   user_age = models.IntegerField()
+   user_location = models.CharField(max_length=10)
+   crypto_price = models.FloatField()
+   market_trend = models.CharField(max_length=7)
+   user_balance = models.FloatField()
